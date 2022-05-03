2999631915274532a22a74d60df3261d1a680534
TSrgy <20205309+TSrgy@users.noreply.github.com>
2022-05-03 18:37:42 +0500

    Add default MAUI app

---
+++ src/FHPathfinder.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FHPathfinder.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
